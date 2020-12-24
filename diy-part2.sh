3ef496cfc694de915beb5410588c663f69c652e1
blackgiulia <1024@protonmail.com>
2020-12-24 13:02:11 +0800

    Add smartdns

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,3 +19,7 @@ git clone https://github.com/vernesong/OpenClash.git package/openclash
 # Add vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+
+# Add smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git package/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git -b lede package/luci-app-smartdns
